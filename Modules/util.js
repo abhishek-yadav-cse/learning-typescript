cd0eca64c8abf483bf1ac4b74f0af14d6a87b4ec
Abhishek Yadav <abhishek.yadav.cs@gmail.com>
2018-08-23 19:06:09 -0700

    undertand using modules in typescript

---
+++ Modules/util.js
@@ -0,0 +1,5 @@
+//console.log(timesTwo(9)); //it will give error
+//it says it cannot find timesTwo()
+/// <reference path = "timesTwo.ts" />
+var use = new Utility.useful();
+console.log(use.timesTwo(9));
---
+++ Modules/util.ts
@@ -0,0 +1,8 @@
+//console.log(timesTwo(9)); //it will give error
+
+//it says it cannot find timesTwo()
+
+
+/// <reference path = "timesTwo.ts" />
+var use = new Utility.useful();
+console.log(use.timesTwo(9));
